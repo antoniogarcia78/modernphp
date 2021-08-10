d2a036888cd89c0a23820a53f8718fda16302f43
Antonio Garcia Marin <antoniogarciamarin@gmail.com>
2021-08-10 14:01:51 +0200

    Modificar validator.php con cambios PHP 7.4

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Acme\Validation;
 
 use Respect\Validation\Validator as Valid;
 
 class Validator
 {
-    public function isValid($validation_data)
+    public function isValid(array $validation_data): array
     {
 
         $errors = [];
@@ -20,19 +21,19 @@ class Validator
                 switch ($exploded[0]) {
                     case 'min':
                         $min = $exploded[1];
-                        if (Valid::string()->length($min)->Validate($_REQUEST[$name]) == false) {
+                        if (Valid::string()->length($min)->Validate($_REQUEST[$name]) === false) {
                             $errors[] = $name . " must be at least " . $min . " characters long!";
                         }
                         break;
 
                     case 'email':
-                        if (Valid::email()->Validate($_REQUEST[$name]) == false){
+                        if (Valid::email()->Validate($_REQUEST[$name]) === false) {
                             $errors[] = $name . " must be a valid email!";
                         }
                         break;
 
                     case 'equalTo':
-                        if (Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false) {
+                        if (Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) === false) {
                             $errors[] = "Value does not match verification value!";
                         }
                         break;
@@ -41,7 +42,9 @@ class Validator
                         $model = "Acme\\models\\" . $exploded[1];
                         $table = new $model;
                         $results = $table::where($name, '=', $_REQUEST[$name])->get();
-                        foreach ($results as $item) {
+                        // Quitamos el foreach y cambiamos por for
+                        //foreach ($results as $item) {
+                        for ($i = 0, $max = count($results); $i < $max; $i++) {
                             $errors[] = $_REQUEST[$name] . " already exists in this system!";
                         }
                         break;
